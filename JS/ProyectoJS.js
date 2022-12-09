231154a71903f67c5efc871a41231e1784bf6e36
jimmysm1995 <118803501+jimmysm1995@users.noreply.github.com>
2022-12-09 13:02:19 +0100

    Add files via upload

---
+++ JS/ProyectoJS.js
@@ -0,0 +1,13 @@
+
+
+/************ Funcion para barra de navegacion***************/
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
+}
